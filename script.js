296504fb42ffe74a375159e07114b584b99cd14b
JamieYau <jamie.yau2002@me.com>
2022-09-03 22:12:30 +0100

    Add multiply function

--- script.js
+++ script.js
@@ -6,5 +6,10 @@ function subtract(num1, num2) {
     return num1 - num2;
 }
 
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
 console.log(add(1, 6));
 console.log(subtract(1, 6));
+console.log(multiply(5, 9));
