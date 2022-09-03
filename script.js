2510e8a1c6b34b2926ca0216106f45e5d92fe8d9
JamieYau <jamie.yau2002@me.com>
2022-09-03 22:13:56 +0100

    Create divide function

--- script.js
+++ script.js
@@ -10,6 +10,11 @@ function multiply(num1, num2) {
     return num1 * num2;
 }
 
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
 console.log(add(1, 6));
 console.log(subtract(1, 6));
 console.log(multiply(5, 9));
+console.log(divide(5, 9));
