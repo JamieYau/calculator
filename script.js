754e236527a5e84308459e05db94161404922d58
JamieYau <jamie.yau2002@me.com>
2022-09-03 22:18:56 +0100

    Create operator function

--- script.js
+++ script.js
@@ -3,18 +3,33 @@ function add(num1, num2) {
 }
 
 function subtract(num1, num2) {
-    return num1 - num2;
+  return num1 - num2;
 }
 
 function multiply(num1, num2) {
-    return num1 * num2;
+  return num1 * num2;
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+  return num1 / num2;
+}
+
+function operator(operator, num1, num2) {
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+    case "*":
+      return multiply(num1, num2);
+    case "/":
+      return divide(num1, num2);
+  }
 }
 
 console.log(add(1, 6));
 console.log(subtract(1, 6));
 console.log(multiply(5, 9));
 console.log(divide(5, 9));
+
+console.log(operator("/", 1, 6));
